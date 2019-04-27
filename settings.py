7d4425dfe532d713e21873e27d2b6f1e5e98875e
Richardlau <48968819+RichardlauCx@users.noreply.github.com>
2019-04-28 00:38:07 +0800

    Update settings.py
    Modify some parameters，Personalize the bullet

--- settings.py
+++ settings.py
@@ -5,8 +5,8 @@ class Settings():  # 存储外星人入侵项目所有设置的类
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
 
-        self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.bullet_speed_factor = 6
+        self.bullet_width = 6
         self.bullet_height = 15
         self.bullet_color = 99, 99, 99
         self.bullets_allowed = 60
@@ -14,5 +14,5 @@ class Settings():  # 存储外星人入侵项目所有设置的类
         self.thano_speed_factor = 1  # 泰坦飞船设置
         self.fleet_drop_speed = 6
         self.fleet_direction = 1  # 为1时表示向右移动，为-1时表示向左移动
-        #available_space_x = first_settings.screen_width - (2 * thanos_width)
-        #number_thanos_x = available_space_x / (2 * thanos_width)
+        # available_space_x = first_settings.screen_width - (2 * thanos_width)
+        # number_thanos_x = available_space_x / (2 * thanos_width)
